341898363df2d44296d377baa5293491c2ae0e78
David Bremner <dbremner@gmail.com>
2018-04-21 10:07:25 -0700

    group prototypes by file

--- fp.h
+++ fp.h
@@ -23,15 +23,41 @@ enum class obj_type {
 
 #include "types.h"
 
+//ast.c
 ast_ptr ast_alloc(int atag, ast_ptr l, ast_ptr m, ast_ptr r);
-obj_ptr obj_alloc(obj_type);
-obj_ptr execute(ast_ptr  act, obj_ptr obj);
-obj_ptr invoke(sym_ptr def, obj_ptr obj);
 void ast_freetree(ast_ptr p);
-[[noreturn]] void fatal_err(const char *msg);
+
+//charfn.c
+obj_ptr do_charfun(ast_ptr act, obj_ptr obj);
+obj_ptr eqobj(obj_ptr obj);
+obj_type numargs(obj_ptr obj);
+
+//defun.c
+obj_ptr invoke(sym_ptr def, obj_ptr obj);
 void defun(sym_ptr name, ast_ptr def);
+
+//exec.c
+obj_ptr execute(ast_ptr  act, obj_ptr obj);
+
+//intrin.c
+int listlen(obj_ptr p);
+obj_ptr do_intrinsics(sym_ptr act, obj_ptr obj);
+
+//lex.c
+void set_prompt(char ch);
+int yylex(void);
+
+//misc.c
+[[noreturn]] void fatal_err(const char *msg);
+void yyerror(const char *msg);
+
+//obj.c
+obj_ptr obj_alloc(obj_type);
 void obj_prtree(obj_ptr p);
 void obj_unref(obj_ptr p);
+
+//YACC runtime
+int yyparse(void);
 
 ///generates the undefined object & returns it
 static inline obj_ptr undefined(void)
@@ -56,22 +82,3 @@ static inline obj_ptr cadr_(obj_ptr x)
 {
     return car_(cdr_(x));
 }
-
-//charfn.c
-obj_ptr do_charfun(ast_ptr act, obj_ptr obj);
-obj_ptr eqobj(obj_ptr obj);
-obj_type numargs(obj_ptr obj);
-
-//intrin.c
-int listlen(obj_ptr p);
-obj_ptr do_intrinsics(sym_ptr act, obj_ptr obj);
-
-//lex.c
-void set_prompt(char ch);
-int yylex(void);
-
-//misc.c
-void yyerror(const char *msg);
-
-//YACC runtime
-int yyparse(void);
