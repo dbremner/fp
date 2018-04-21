240a8d3b7b3b77356f811a5c55caa16403b40a71
David Bremner <dbremner@gmail.com>
2018-04-21 13:37:29 -0700

    remove use of local variable

--- ast.c
+++ ast.c
@@ -22,7 +22,7 @@ ast_alloc(int atag, ast_ptr l, ast_ptr m, ast_ptr r)
 {
     inc_count();
     ast_ptr p = ast_list;
-    if(p){
+    if(ast_list){
         ast_list = p->left;
         p->init(atag, l, m, r);
         return( p );
