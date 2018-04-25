1e8bd871d1667f56d0ae09e2940aa2f1f01aae87
David Bremner <dbremner@gmail.com>
2018-04-25 09:25:44 -0700

    move prototypes to charfn.h

---
+++ charfn.h
@@ -0,0 +1,8 @@
+#ifndef CHARFN_H
+#define CHARFN_H
+
+obj_ptr do_charfun(ast_ptr act, obj_ptr obj);
+obj_ptr eqobj(obj_ptr obj);
+obj_type numargs(obj_ptr obj);
+
+#endif
--- fp.h
+++ fp.h
@@ -22,10 +22,7 @@ ast_ptr ast_alloc(int atag, ast_ptr l);
 ast_ptr ast_alloc(int atag, ast_ptr l, ast_ptr m, ast_ptr r);
 void ast_freetree(ast_ptr p);
 
-//charfn.c
-obj_ptr do_charfun(ast_ptr act, obj_ptr obj);
-obj_ptr eqobj(obj_ptr obj);
-obj_type numargs(obj_ptr obj);
+#include "charfn.h"
 
 //defun.c
 void defun(sym_ptr name, ast_ptr def);
