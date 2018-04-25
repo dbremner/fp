f7a1f76a714b4e942628170703d4a68d6bd6d70a
David Bremner <dbremner@gmail.com>
2018-04-24 22:47:24 -0700

    move symtab_entry definition to symtab_entry.hpp

---
+++ symtab_entry.hpp
@@ -0,0 +1,24 @@
+#ifndef SYMTAB_ENTRY_HPP
+#define SYMTAB_ENTRY_HPP
+
+/// A symbol table entry for an identifier
+struct symtab_entry {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-private-field"
+private:
+    unsigned padding = 0;
+#pragma clang diagnostic pop
+public:
+    symtype sym_type;
+    YYstype sym_val{};
+    sym_ptr sym_next = nullptr;
+    const std::string sym_pname;
+    
+    symtab_entry(const char *pname)
+    :   sym_type{symtype::SYM_NEW},
+    sym_pname{pname}
+    {
+    }
+};
+
+#endif
--- types.h
+++ types.h
@@ -56,24 +56,6 @@ public:
 
 #include "symtype.hpp"
 
-/// A symbol table entry for an identifier
-struct symtab_entry {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-private-field"
-private:
-    unsigned padding = 0;
-#pragma clang diagnostic pop
-public:
-    symtype sym_type;
-    YYstype sym_val{};
-    sym_ptr sym_next = nullptr;
-    const std::string sym_pname;
-    
-    symtab_entry(const char *pname)
-    :   sym_type{symtype::SYM_NEW},
-        sym_pname{pname}
-    {
-    }
-};
+#include "symtab_entry.hpp"
 
 #endif
