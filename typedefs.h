40d25d45bbe296577cf21724561372056dbdd538
David Bremner <dbremner@gmail.com>
2018-04-25 14:37:45 -0700

    change typedef formatting

--- typedefs.h
+++ typedefs.h
@@ -1,9 +1,9 @@
 #ifndef TYPEDEFS_H
 #define TYPEDEFS_H
 
-typedef struct ast* ast_ptr;
-typedef struct list* list_ptr;
-typedef struct object* obj_ptr;
-typedef struct symtab_entry* sym_ptr;
+typedef struct ast *ast_ptr;
+typedef struct list *list_ptr;
+typedef struct object *obj_ptr;
+typedef struct symtab_entry *sym_ptr;
 
 #endif
