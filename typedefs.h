22e36fee8a79a0bd222aa0def15e07e31e30f9e3
David Bremner <dbremner@gmail.com>
2018-04-25 15:39:01 -0700

    add typedefs for non null pointers

--- typedefs.h
+++ typedefs.h
@@ -2,8 +2,14 @@
 #define TYPEDEFS_H
 
 typedef struct ast * _Nullable ast_ptr;
+typedef struct ast * _Nonnull live_ast_ptr;
+
 typedef struct list * _Nullable list_ptr;
+
 typedef struct object * _Nullable obj_ptr;
+typedef struct object * _Nonnull live_obj_ptr;
+
 typedef struct symtab_entry * _Nullable sym_ptr;
+typedef struct symtab_entry * _Nonnull live_sym_ptr;
 
 #endif
