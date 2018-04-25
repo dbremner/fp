013b0b06f766fad2f563b38e61bedf9589905c96
David Bremner <dbremner@gmail.com>
2018-04-25 15:25:25 -0700

    move main to main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "fpcommon.h"
+#include "lex.h"
+#include "signal_handling.h"
+#include "symtab.h"
+
+jmp_buf restart;
+
+//YACC runtime
+int yyparse(void);
+
+int
+main(void) {
+    symtab_init();
+    set_prompt('\t');
+    
+    set_signal_handlers();
+    
+    if( setjmp(restart) == 0 )
+        printf("FP v0.0\n");
+    else
+        printf("FP restarted\n");
+    yyparse();
+    printf("\nFP done\n");
+    exit(EXIT_SUCCESS);
+}
--- misc.c
+++ misc.c
@@ -3,17 +3,12 @@
  *
  *	Copyright (c) 1986 by Andy Valencia
  */
-#include <setjmp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "fpcommon.h"
 #include "lex.h"
 #include "misc.h"
-#include "signal_handling.h"
-#include "symtab.h"
-
-jmp_buf restart;
 
 [[noreturn]] void
 fatal_err(const char *msg)
@@ -31,23 +26,4 @@ yyerror(const char *msg)
     assert(strlen(msg) > 0);
     printf("yyerror() reports '%s'\n",msg);
     set_prompt('\t');
-}
-
-//YACC runtime
-int yyparse(void);
-
-int
-main(void) {
-    symtab_init();
-    set_prompt('\t');
-
-    set_signal_handlers();
-
-    if( setjmp(restart) == 0 )
-	printf("FP v0.0\n");
-    else
-	printf("FP restarted\n");
-    yyparse();
-    printf("\nFP done\n");
-    exit(EXIT_SUCCESS);
 }
