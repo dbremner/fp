f21a35b727ec30377a5bb6559da119641760f365
David Bremner <dbremner@gmail.com>
2018-04-24 22:51:28 -0700

    move obj.c prototypes to obj.h

--- fp.h
+++ fp.h
@@ -40,20 +40,7 @@ int yylex(void);
 [[noreturn]] void fatal_err(const char *msg);
 void yyerror(const char *msg);
 
-//obj.c
-obj_ptr obj_alloc(obj_type);
-/// constructs a T_INT
-obj_ptr obj_alloc(int value);
-/// constructs a T_BOOL
-obj_ptr obj_alloc(bool value);
-/// constructs a T_FLOAT
-obj_ptr obj_alloc(double value);
-/// constructs a T_LIST
-obj_ptr obj_alloc(obj_ptr car_, obj_ptr cdr_ = nullptr);
-///generates the undefined object & returns it
-obj_ptr undefined(void);
-void obj_prtree(obj_ptr p);
-void obj_unref(obj_ptr p);
+#include "obj.h"
 
 //YACC runtime
 int yyparse(void);
--- obj.h
+++ obj.h
@@ -1,1 +1,19 @@
+#ifndef OBJ_H
+#define OBJ_H
 
+//obj.c
+obj_ptr obj_alloc(obj_type);
+/// constructs a T_INT
+obj_ptr obj_alloc(int value);
+/// constructs a T_BOOL
+obj_ptr obj_alloc(bool value);
+/// constructs a T_FLOAT
+obj_ptr obj_alloc(double value);
+/// constructs a T_LIST
+obj_ptr obj_alloc(obj_ptr car_, obj_ptr cdr_ = nullptr);
+///generates the undefined object & returns it
+obj_ptr undefined(void);
+void obj_prtree(obj_ptr p);
+void obj_unref(obj_ptr p);
+
+#endif
