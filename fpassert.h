bc813f8068874478db426b162ebf638d8f36ce88
David Bremner <dbremner@gmail.com>
2018-04-25 14:41:14 -0700

    move comment

--- fpassert.h
+++ fpassert.h
@@ -1,12 +1,6 @@
 #ifndef FPASSERT_H
 #define FPASSERT_H
 
-/*
- * Common definitions for FP
- *
- *	Copyright (c) 1986 by Andy Valencia
- */
-
 //enable assertions in all builds
 #undef NDEBUG
 #include <assert.h>
--- fpcommon.h
+++ fpcommon.h
@@ -1,6 +1,12 @@
 #ifndef FPCOMMON_H
 #define FPCOMMON_H
 
+/*
+ * Common definitions for FP
+ *
+ *    Copyright (c) 1986 by Andy Valencia
+ */
+
 #include "fpassert.h"
 #include "typedefs.h"
 
