8a9b5ae0a7fd4cf2405d2f3ca681a0f569ba2011
David Bremner <dbremner@gmail.com>
2018-04-29 11:31:13 -0700

    add accessors that assert and cast

--- ast.hpp
+++ ast.hpp
@@ -9,6 +9,27 @@ struct ast {
     ast_ptr middle = nullptr;
     ast_ptr right = nullptr;
     
+    live_ast_ptr live_left()
+    {
+        assert(left);
+        auto result = static_cast<live_ast_ptr>(left);
+        return result;
+    }
+    
+    live_ast_ptr live_middle()
+    {
+        assert(middle);
+        auto result = static_cast<live_ast_ptr>(middle);
+        return result;
+    }
+    
+    live_ast_ptr live_right()
+    {
+        assert(right);
+        auto result = static_cast<live_ast_ptr>(right);
+        return result;
+    }
+    
     ast(int tag_, ast_ptr left_, ast_ptr middle_, ast_ptr right_)
     {
         tag = tag_;
